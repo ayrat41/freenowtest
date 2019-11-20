065ee9640b51b4a90cdbf5550102cef3c9671053
Airat Aibedullov <aaibedul@Airats-MBP.fios-router.home>
2019-11-20 03:18:25 -0500

    new

--- comfreeNowapitest/src/test/java/steps/StepsDefinition.java
+++ comfreeNowapitest/src/test/java/steps/StepsDefinition.java
@@ -5,6 +5,8 @@ import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
 import org.testng.annotations.Test;
 import util.Util;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -29,36 +31,57 @@ public class StepsDefinition extends Util {
         List <String> userNames  = jsonPathEvaluator.getList("username");
         for (int i=0; i<userNames.size(); i++) {
             if (userNames.get(i).trim().equalsIgnoreCase(name)) {
-                System.out.println("Test emails in comments for user  ==> " + userNames.get(i).trim());
+                System.out.println("Defined user for testing " + userNames.get(i).trim());
                 userId = jsonPathEvaluator.getInt("id["+i+"]");
                 break;
             }
         }
     }
 
+    @Test(dependsOnMethods = { "find_userId_contains_username_use_to_the_next_scenario" })
+    public void find_posts_contains_userId_use_to_the_next_scenario() {
+        String url = Util.prop.getProperty("URL");
+        String api = Util.prop.getProperty("postsUri");
+        String endPointUri = url+api;
+        System.out.println(endPointUri);
+        String name = Util.prop.getProperty("userId");
+        response = RestAssured.given().when().get(endPointUri + "?userId=" + userId);
+        response.then().assertThat().statusCode(200);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        postIds = new ArrayList<Integer>();
+        postIds.addAll(jsonPathEvaluator.getList("id"));
+        System.out.println("List of postIds for defined user created");
+    }
+
 
 // With this function we call /comments api using userId parameter from previos method
 // Get all emails from comments for this specific user
 // Check if email syntax is valid using method in Util class
-    @Test (dependsOnMethods = { "find_userId_contains_username_use_to_the_next_scenario" })
+    @Test (dependsOnMethods = { "find_posts_contains_userId_use_to_the_next_scenario" })
     public void find_comments_contains_userId_check_emails() {
         String url = Util.prop.getProperty("URL");
         String api = Util.prop.getProperty("commentsUri");
-        String endPointUri = url + api + "?user=" + userId;
+        String endPointUri = url + api;
         System.out.println(endPointUri);
-        response = RestAssured.given().parameter("user", userId).get(endPointUri);
-        response.then().assertThat().statusCode(200);
-        JsonPath jsonPathEvaluator = response.jsonPath();
-        List<String> commentemails = jsonPathEvaluator.getList("email");
-        int status = 0;
-        for (int i=0; i<commentemails.size(); i++) {
-            if (!Util.isValid(commentemails.get(i))) {
-                System.out.println("The email ==> " + commentemails.get(i) + " is not valid");
-                status=status+1;
-            }
+        List<String> commentemails = new ArrayList<String>();
+        for(int i =0; i<this.postIds.size(); i++) {
+            response = RestAssured.given().when().get(endPointUri + "?postId=" + postIds.get(i));
+            response.then().assertThat().statusCode(200);
+            JsonPath jsonPathEvaluator = response.jsonPath();
+            commentemails.addAll(jsonPathEvaluator.getList("email"));
         }
-        if (status==0){
-            System.out.println("ALL EMAILS ARE VALID");
+            int status = 0;
+            for (int n = 0; n < commentemails.size(); n++) {
+                if (!Util.isValid(commentemails.get(n))) {
+                    System.out.println("The email ==> " + commentemails.get(n) + " is not valid");
+                    status = status + 1;
+                } else {
+                    System.out.println(n+". The email ==> " + commentemails.get(n) + " is valid");
+                }
+            }
+            if (status == 0) {
+                System.out.println("All emails for defined user are valid");
+
         }
     }
 
